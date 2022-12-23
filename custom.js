30d15d2880677b55aa3907c494b073cd14b1a7d4
Ciman Code <firmanabdul278@gmail.com>
2022-12-23 22:04:58 +0700

    add filer javascript

---
+++ custom.js
@@ -0,0 +1,11 @@
+function today(){
+    let nama = "Hanif";
+    if(nama == "Firam"){
+        console.log("Hallo "+nama)
+    }else if(nama == "Hanif") {
+        console.log("Hallo "+nama)
+    } else {
+        console.log("Hallo "+nama)
+    }
+}
+return today();
